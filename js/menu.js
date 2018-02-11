f569b69273af379669355745b0bd37ab5a5199b1
Rozitta <rozitta2009@gmail.com>
2018-02-11 17:54:40 +0200

    Add 2 new pages in the adaptive version

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,22 @@
  // активный пункт меню
  var blocks = []
- $(window).scroll(function () {
-     var scrolledpx = parseInt($(window).scrollTop()) + (parseInt($(window).height()) / 1.5);
-     var id = '';
-     for (var i = 0; i < blocks.length; i++) {
+//  $(window).scroll(function () {
+//      var scrolledpx = parseInt($(window).scrollTop()) + (parseInt($(window).height()) / 1.5);
+//      var id = '';
+//      for (var i = 0; i < blocks.length; i++) {
 
-         if (scrolledpx >= blocks[i].position.top) {
-             id = blocks[i].id;
-             if (blocks[i + 1] !== undefined && scrolledpx < blocks[i + 1].position.top) {
-                 id = blocks[i].id;
-             }
-         }
-     };
-     $('.navbar__link').removeClass("navbar__link--active");
-     $("[data-action='" + id + "_menu']").addClass("navbar__link--active");
+//          if (scrolledpx >= blocks[i].position.top) {
+//              id = blocks[i].id;
+//              if (blocks[i + 1] !== undefined && scrolledpx < blocks[i + 1].position.top) {
+//                  id = blocks[i].id;
+//              }
+//          }
+//      };
+    //  $('.navbar__link').removeClass("navbar__link--active");
+    //  $("[data-action='" + id + "_menu']").addClass("navbar__link--active");
 
- });
+//  }
+// );
 
  $(function () {
      blocks.push({
@@ -34,10 +35,9 @@
          id: 'contacts',
          position: $('#contacts').offset()
      })
-
  });
 
- //плавный переход по ссылке
+ //плавный переход по ссылке навигации
  $(document).ready(function () {
      $(".navbar__menu").each(function (index, elem) {
          $(elem).on("click", "a", function (event) {
@@ -54,6 +54,24 @@
          });
      })
  });
+
+//плавный переход по ссылке к нужному месту по якорю
+ $(document).ready(function () {
+    $(".registration__part").each(function (index, elem) {
+        $(elem).on("click", "a", function (event) {
+            //отменяем стандартную обработку нажатия по ссылке
+            event.preventDefault();
+            //забираем идентификатор бока с атрибута href
+            var id = $(this).attr('href'),
+                //узнаем высоту от начала страницы до блока на который ссылается якорь
+                top = $(id).offset().top;
+            //анимируем переход на расстояние - top за 1500 мс
+            $('body,html').animate({
+                scrollTop: top
+            }, 800);
+        });
+    })
+});
 
  $(function () {
      var openBtn = $('.navbar__burger'),
